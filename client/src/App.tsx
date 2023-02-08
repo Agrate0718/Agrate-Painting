554ddb6e34c8a9a6beaf0a9f8d76cb50e1f81f43
Agrate0718 <zionagrate@gmail.com>
2023-02-07 21:34:55 -0500

    set up routes for landing page and applied NavBar to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/Pages/LandingPage";
+import NavBar from "./components/Partials/NavBar";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="">
+        <header className=" py-9">
+          <NavBar />
+        </header>
+        <main className="bg-green-500 relative">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </main>
+      </div>
+      {/* <footer className="border-4 mt-96 border-orange-800">footer</footer> */}
+    </Router>
   );
 }
-
-export default App;
