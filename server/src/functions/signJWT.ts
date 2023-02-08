2fc2cb0e7074ac9ccce1f812b7be393219e8f998
Agrate0718 <zionagrate@gmail.com>
2023-02-07 20:53:18 -0500

    made signJWT function to sign a token for a user

---
+++ server/src/functions/signJWT.ts
@@ -0,0 +1,41 @@
+import jwt from 'jsonwebtoken';
+import { config } from '../config/config';
+import Logging from '../library/Logging';
+import logging from '../library/loggings';
+import { IUser } from '../models/User';
+
+let NAMESPACE = 'Auth';
+
+const signJWT = (user: IUser, callback: (error: Error | null, token: string | null) => void): void => {
+    var timeSinchEpoch = new Date().getTime();
+    var expirationTime = timeSinchEpoch + Number(config.server.token.expireTime) * 100000;
+    var expirationTimeInSeconds = Math.floor(expirationTime / 1000);
+
+    Logging.info(`Attempting to sign token for ${user.firstName}`);
+
+    try {
+        jwt.sign(
+            {
+                firstName: user.firstName
+            },
+            config.server.token.secret,
+            {
+                issuer: config.server.token.issuer,
+                algorithm: 'HS256',
+                expiresIn: expirationTimeInSeconds
+            },
+            (error, token) => {
+                if (error) {
+                    callback(error, null);
+                } else if (token) {
+                    callback(null, token);
+                }
+            }
+        );
+    } catch (error: any) {
+        logging.error(NAMESPACE, error.message, error);
+        callback(error, null);
+    }
+};
+
+export default signJWT;
