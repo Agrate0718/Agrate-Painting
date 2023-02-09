46489eeb06fa8140030d54e33c14333975be9ef5
Agrate0718 <zionagrate@gmail.com>
2023-02-08 22:01:39 -0500

    made and api call for all artworks

--- client/src/test-folder/Test.tsx
+++ client/src/test-folder/Test.tsx
@@ -1,11 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Test() {
   console.log("test page");
   const server = process.env.REACT_APP_SERVER_URL;
-  console.log(server);
+  //   const allArtworks =
+  //   console.log(allArtworks);
 
+  // Artwork from the backend
+  const [artworks, setArtworks] = useState([]);
+  // State for error message from backend
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const getArtworks = async () => {
+      try {
+        const response = await axios.get(`${server}/artworks/get`);
+        const artData = response.data.artworks;
+        console.log("response:", response);
+        setArtworks(artData);
+      } catch (err: any) {
+        console.warn(err);
+        if (err.response) {
+          setErrorMessage(err.response.data.message);
+        }
+      }
+    };
+    getArtworks();
+  }, []);
+  console.log("artworks:", artworks);
   return (
     <div>
-      <p>Test Page</p>
+      <p>Test h Page</p>
     </div>
   );
 }
