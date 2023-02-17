8fbdce146d375d96998233d40b6357911c91ce39
Agrate0718 <zionagrate@gmail.com>
2023-02-16 23:23:53 -0500

    made more progress setting up the login page

--- client/src/components/Pages/LoginPage.tsx
+++ client/src/components/Pages/LoginPage.tsx
@@ -46,72 +46,118 @@ export default function LoginPage({
     }
   };
   return (
-    <div className="flex  items-center justify-center my-20">
-      <div className="  grid gap-5 w-10/12  grid-cols-2   place-items-center">
-        <section className="h-screen">
-          <div className="container px-6 py-12 h-full">
-            <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-              <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0"></div>
-
-              <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
-                <form onSubmit={handleSubmit}>
-                  <p>Login:</p>
-                  <br></br>
-
-                  {/* <!-- Email input --> */}
-                  <div className="mb-6 ">
-                    <input
-                      type="email"
-                      id="email"
-                      className=""
-                      placeholder="Email address"
-                      onChange={(e) => setEmail(e.target.value)}
-                      value={email}
-                      required
-                    />
-                  </div>
-
-                  {/* <!-- Password input --> */}
-                  <div className="mb-6">
-                    <input
-                      type="password"
-                      id="password"
-                      className=""
-                      placeholder="Password"
-                      onChange={(e) => setPassword(e.target.value)}
-                      value={password}
-                      required
-                    />
-                  </div>
-
-                  {/* <!-- Submit button --> */}
-                  <button
-                    type="submit"
-                    className="bg-red-500 rounded"
-                    data-mdb-ripple="true"
-                    data-mdb-ripple-color="light"
-                  >
-                    Sign in
-                  </button>
-
-                  {/* <div
-									className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
-								>
-									<p className="text-center font-semibold mx-4 mb-0">OR</p>
-								</div> */}
-                </form>
-              </div>
-            </div>
-          </div>
-        </section>
-        <div className="bg-green-500 grid gap-5 grid-cols-2 ">
-          <div> Register----Column two.one</div>
-          <div>Column two.two</div>
-          <div className="grid row-span-2">
-            CHickenAppleewuvfbhuelgbuieghoie4jgoijhgijrsh;jrsihojrhoirdjhoijsrhoiSauce
-          </div>
+    // <div className="flex flex-wrap items-center justify-center my-20 w-[484] h-[1240px]">
+    <div className="  grid gap-20 w-[484]  grid-cols-2   place-items-center h-[1240px] bg-blue-200">
+      <form
+        className=" w-full flex justify-center items-center flex-wrap  bg-red-500"
+        onSubmit={handleSubmit}
+      >
+        {/* <!-- Email input --> */}
+        <div className="mb-6 ">
+          <p className="mb-.1">Login</p>
+          <p className="mb-.1">Email Address *</p>
+          <input
+            type="email"
+            id="email"
+            className="w-[500px] h-[42px] border-solid border-2 border-black  p-2.5 "
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
         </div>
+
+        {/* <!-- Password input --> */}
+        <div className="mb-6">
+          <p>Password *</p>
+          <input
+            type="password"
+            id="password"
+            className="w-[500px] h-[42px] border-solid border-2 border-black  p-2.5"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            required
+          />
+        </div>
+
+        {/* <!-- Submit button --> */}
+        <button
+          type="submit"
+          className="w-[500px] h-[42px] bg-blue-400 rounded  "
+          data-mdb-ripple="true"
+          data-mdb-ripple-color="light"
+        >
+          Sign in
+        </button>
+      </form>
+
+      <div className=" ">
+        <form
+          className="  bg-orage-500 w-full flex justify-center items-center flex-wrap grid grid-cols-2 grid-cols-[300px]"
+          onSubmit={handleSubmit}
+        >
+          {/* <!-- First Name input --> */}
+          <p className="mb  col-span-2 ">Register</p>
+          <div className="mb-6 ">
+            <p className="mb-.1">First Name *</p>
+            <input
+              type="email"
+              id="email"
+              className="w-[235px] h-[42px] border-solid border-2 border-black  p-2.5 "
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
+            />
+          </div>
+          {/* <!-- Last Name input --> */}
+          <div className="mb-6 ">
+            <p className="mb-.1">Last Name *</p>
+            <input
+              type="email"
+              id="email"
+              className="w-[235px] h-[42px] border-solid border-2 border-black  p-2.5 "
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
+            />
+          </div>
+          {/* <!-- Email input --> */}
+          <div className="mb-6 col-end-3 col-span-2 border-2 ">
+            <p className="mb-.1">Email Address *</p>
+            <input
+              type="email"
+              id="email"
+              className="w-[500px] h-[42px] border-solid  border-black  p-2.5 "
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
+            />
+          </div>
+
+          {/* <!-- Password input --> */}
+          <div className="mb-6 col-end-3 col-span-2 border-2">
+            <p>Password *</p>
+            <input
+              type="password"
+              id="password"
+              className="w-[500px] h-[42px] border-solid  border-black  p-2.5"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              required
+            />
+          </div>
+
+          {/* <!-- Submit button --> */}
+          <button
+            type="submit"
+            className="w-[500px] h-[42px] bg-blue-400 rounded  "
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+          >
+            Sign in
+          </button>
+        </form>
       </div>
     </div>
+    // </div>
   );
 }
