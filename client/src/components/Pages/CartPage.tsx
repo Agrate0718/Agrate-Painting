9216a8ea36498e1ce3744ff1927c8f35f4d5a364
Agrate0718 <zionagrate@gmail.com>
2023-03-25 17:55:02 -0400

    set up grid layout for shopping cart UI

--- client/src/components/Pages/CartPage.tsx
+++ client/src/components/Pages/CartPage.tsx
@@ -1,7 +1,115 @@
 export default function CartPage() {
   return (
-    <div className="h-screen w-full bg-orange-500">
-      <p>cart page</p>
+    <div className="h-[100px] w-[ful] bg-orang-500 flex justify-center items-center p-[200px] ">
+      <div className="  grid grid-cols-[78px,135px,331px,257px,175px,262px] w-[1240px] h-[37p] text-[16px]  ">
+        <span className="w-[78px] h-[37px] bg-black "></span>
+        <span className="w-[135px] h-[30p] bg-black"></span>
+        <div className="w-[331.11px] h-[37p] bg-black  text-white flex font-bold items-center">
+          Product
+        </div>
+        <div className="w-[257.42px] h-[37p] bg-black text-white flex font-bold items-center">
+          Price
+        </div>
+        <div className="w-[175.88px] h-[37p] bg-black text-white flex font-bold items-center">
+          Quantity
+        </div>
+        <div className="w-[262.3px] h-[37p] bg-black text-white flex font-bold items-center">
+          Total
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black w-[25p] h-[25p]">
+          <div className="w-[25px] h-[25px] hover:bg-black hover:text-white hover:font-semibold text-center rounded-xl">
+            X
+          </div>
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        {/* /////// */}
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          Apple bottom Jeans
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+        <div className="py-2.5 px-1.5 border-b-[1px] border-black h-[62px]">
+          X
+        </div>
+      </div>
     </div>
   );
 }
